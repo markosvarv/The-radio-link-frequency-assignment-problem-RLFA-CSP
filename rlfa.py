5ee67559a2ca6dffe238bd5cecaf0f74a810f61c
Mark Varvagiannis <markosvarv@gmail.com>
2021-02-13 13:53:07 +0200

    finish constraint dicts and function

--- rlfa.py
+++ rlfa.py
@@ -29,7 +29,14 @@ def get_constraint_list(containts_str):
     constraints_number = int(constraint_list.pop(0))
     if constraints_number != len(constraint_list):
         raise ValueError("Wrong lines input")
-    return constraint_list
+
+    splitted_constraints_list = []
+    for node in constraint_list:
+        #split the list into separate strings
+        current_splitted_constraints_list = node.split()
+        splitted_constraints_list.append(current_splitted_constraints_list)
+
+    return splitted_constraints_list
 
 def assign_domains_to_variables(variables_dict, domains_dict):
     variables_domain_dict = {}
@@ -37,7 +44,48 @@ def assign_domains_to_variables(variables_dict, domains_dict):
         variables_domain_dict[variable] = domains_dict[domain]
     return variables_domain_dict
 
+def get_neighbors_dict(constraints_list):
+    neighbors_dict = {}
+    for current_list in constraints_list:
+        x = int(current_list[0])
+        y = int(current_list[1])
 
+        # initialize the variables lists for the current key if it does not exist in the dict
+        if x not in neighbors_dict:
+            neighbors_dict[x] = []
+        if y not in neighbors_dict:
+            neighbors_dict[y] = []
+
+        #assign the values
+        neighbors_dict[x].append(y)
+        neighbors_dict[y].append(x)
+
+    return neighbors_dict
+
+def get_constraints_dict(constraints_list):
+    constraints_dict = {}
+    for current_list in constraints_list:
+        x = int(current_list[0])
+        y = int(current_list[1])
+        comp =  current_list[2]
+        k = int(current_list[3])
+
+        #assign the values
+        if (x,y) in constraints_dict:
+            print ("More than one constraints for the same variables. Only the last one will remain.")
+
+        constraints_dict[(x,y)] = (comp, k)
+
+    return constraints_dict
+
+def constraints_function(A, a, B, b, constraints_dict):
+    comp, k = constraints_dict[(A,B)]
+    if comp == '>':
+        return abs(a - b) > k
+    elif comp == '=':
+        return abs(a - b) == k
+    else:
+        raise ValueError("Wrong compare operator")
 
 instance = "11"
 domains_file = open("rlfap/dom" + instance + ".txt", "r")
@@ -48,10 +96,16 @@ domains_dict = get_domains_dict(domains_file.read())
 variables_dict = get_variables_dict(variables_file.read())
 constraints_list = get_constraint_list(constraints_file.read())
 
-print(constraints_list)
-
 variables_list = list(variables_dict.keys())
 variables_domain_dict = assign_domains_to_variables(variables_dict, domains_dict)
+
+neighbors_dict = get_neighbors_dict(constraints_list)
+
+constraints_dict = get_constraints_dict(constraints_list)
+# print(constraints_dict)
+
+# constraints_function(260, 200, 261, 439, constraints_dict)
+
 
 
 
